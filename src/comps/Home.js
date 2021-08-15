67fec7725165759c63273447b869dc01669f5868
ChandanPk <chandanpk9@gmail.com>
2021-08-15 20:32:35 +0530

    add checks

--- src/comps/Home.js
+++ src/comps/Home.js
@@ -5,68 +5,170 @@ import Calender from '../img/calender.png'
 
 
 const Home = () => {
+    var date;
     const [startDate, setDate] = useState();
-    const [palindrome, setPaladrom] = useState(false);
-
+    const [dateAndDays, setDateAndDays] = useState(false);
+    const [isItPalindrome, setIsItPalindrome] = useState(null);
 
 
     const check = (e)=> {
-        setPaladrom(prevPaladrom => null);
         e.preventDefault();
-       console.log(getAlldateFormats());
-       console.log(checkPalindromeForAlldates())
-
-        
-    }
-
-    const getAlldateFormats = ()=> {
-        
         const d = new Date(startDate);
-        const year = JSON.stringify(d.getFullYear());
-        const month = ("0" + (d.getMonth() + 1)).slice(-2);
-        const date = ("0" + d.getDate()).slice(-2);
+        const yr = JSON.stringify(d.getFullYear());
+        // const mth = ("0" + (d.getMonth() + 1)).slice(-2);
+        const mth = d.getMonth() + 1;
+        const dte = d.getDate();
+        date = {day: dte, month:mth, year: yr};
 
-        const ddmmyyyy = date + month + year;
-        const mmddyyyy = month + date + year;
-        const yyyymmdd = year + month + date;
-        const ddmmyy   = date + month + year.slice(-2);
-        const mmddyy   = month + date + year.slice(-2);
-        const yymmdd   = year.slice(-2) + month + date;
-
-        return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yymmdd];
-        }
-
-    const checkPalindromeForAlldates = ()=> {
-        let listOfPalindromes = getAlldateFormats(); 
-        
-        var isPalindrome = false;
-
-        for(var i = 0; i < listOfPalindromes.length; i++) {
-            if(isItPalindrome(listOfPalindromes[i])){
-                isPalindrome = true;
-                break;
+        if (checkPalindromeForAllDateFormats(date)){
+            if (dateAndDays) {
+                setDateAndDays(null);
             }
-        }
-        return isPalindrome;
-    }
-
-    const isItPalindrome = (str) => {
-        let reverse = reverseString(str);
-        if (str === reverse) {
-            return true;
-        } else {
-            return false;
+            setIsItPalindrome(true);
+        }else {
+            getNextPalindromeDate(date);
         }
     }
 
-    const reverseString = (str) => {
-        let reversedStr = str.split('').reverse().join('');
+    const reverseStr = str => {
+        var listOfChars = str.split('');
+        var reverseListOfChars = listOfChars.reverse();
+        var reversedStr = reverseListOfChars.join('');
         return reversedStr;
-    }
+      }
+      
+    const isPalindrome = str => {
+        var reverse = reverseStr(str);
+        return str === reverse;
+      }
+      
+    const convertDateToStr = date => {
+      
+        var dateStr = { day: '', month: '', year: '' };
+      
+        if (date.day < 10) {
+          dateStr.day = '0' + date.day;
+        }
+        else {
+          dateStr.day = date.day.toString();
+        }
+      
+        if (date.month < 10) {
+          dateStr.month = '0' + date.month;
+        }
+        else {
+          dateStr.month = date.month.toString();
+        }
+      
+        dateStr.year = date.year.toString();
+        return dateStr;
+      }
+      
+    const getAllDateFormats = date => {
+        var dateStr = convertDateToStr(date);
+      
+        var ddmmyyyy = dateStr.day + dateStr.month + dateStr.year;
+        var mmddyyyy = dateStr.month + dateStr.day + dateStr.year;
+        var yyyymmdd = dateStr.year + dateStr.month + dateStr.day;
+        var ddmmyy = dateStr.day + dateStr.month + dateStr.year.slice(-2);
+        var mmddyy = dateStr.month + dateStr.day + dateStr.year.slice(-2);
+        var yymmdd = dateStr.year.slice(-2) + dateStr.month + dateStr.day;
+      
+        return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yymmdd];
+      }
+      
+    const checkPalindromeForAllDateFormats = date => {
+        var listOfPalindromes = getAllDateFormats(date);
+      
+        var flag = false;
+      
+        for(var i=0; i < listOfPalindromes.length; i++){
+          if(isPalindrome(listOfPalindromes[i])){
+            flag = true;
+            break;
+          }
+        }
+        return flag;
+      }
+      
+      // check for leap year
+    const isLeapYear = year => {
+        if(year % 400 === 0){
+          return true;
+        }
+        if(year % 100 === 0){
+          return false;
+        }
+        if(year % 4 === 0){
+          return true;
+        }
+        return false;
+      }
+      
+      // gets next date
+    const getNextDate = date => {
+        var day = date.day + 1; 
+        var month = date.month;
+        var year = date.year;
+      
+        var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]; // 0 - 11
+      
+         // check for february
+        if(month === 2){ 
+          // check for leap year
+          if(isLeapYear(year)){ 
+             if(day > 29){ 
+               day = 1;
+               month++;
+             }
+          }
+          else {
+             if(day > 28){
+               day = 1;
+               month++;
+             }
+          }
+        }
+        
+        else {
 
-
-
-
+          if(day > daysInMonth[month - 1]){ 
+            day = 1; 
+            month++;
+          }
+        }
+      
+        if(month > 12){
+          month = 1;
+          year++; 
+        }
+      
+        return {
+          day: day,  
+          month: month,
+          year: year
+        };
+      }
+      
+      // get next palindrome date
+    const getNextPalindromeDate = date => {
+        var ctr = 0;
+        var nextDate = getNextDate(date);
+      
+        while(1){
+          ctr++;
+          var isPalindrome = checkPalindromeForAllDateFormats(nextDate);
+          if(isPalindrome){
+            break;
+          }
+          nextDate = getNextDate(nextDate);
+        }
+        setDateAndDays([ctr, nextDate]);
+        if (isItPalindrome) {
+            setIsItPalindrome(null);
+        }
+        return [ctr, nextDate];
+      }
 
     return (
         <>
@@ -106,93 +208,11 @@ const Home = () => {
                 <button type="submit" className="btn-2 btn">Check</button>
             </form>
 
+                {isItPalindrome && <p className="results">🥳Hurray!!! Your birday is a palidrome day.🥳</p>}
+                {dateAndDays && <p className="results">🙄Awww! Your birthdate is not palindrome. Nearest palindrome date is {dateAndDays[1].day + "/" + dateAndDays[1].month + "/" + dateAndDays[1].year}. You missed it by {dateAndDays[0]} days.</p>}
+
         </>
     );
 }
 
 export default Home;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- // var count = 0;
-        // var bool = false;
-        // var breakLoop = false;
-        
-        // for (const item in dateFormats){
-        //     var j = 7;
-        //     var i = 0;
-            
-        //     if(bool === true){
-        //         j = 7;
-        //         i = 0;
-        //     }
-            
-        //     for (i; i<=7; i++){
-        //         if (dateFormats[item][i] === dateFormats[item][j]){
-        //             count+= 1;
-        //             // console.log(dateFormats[item][i], dateFormats[item][j]);
-        //             console.log(count); 
-        //         }
-                
-        //         if (i === 7){
-        //             if (count === 8){
-        //                 setPaladrom(prePaladrom => true);
-        //                 breakLoop = true;
-        //                 break;
-        //             }
-        //         } else {  
-        //             bool = true;
-        //         }
-                
-        //         j--;            
-               
-        //     }
-        //     console.log(dateFormats[item], "break");
-        //     if (breakLoop){
-        //         break;
-        //     }
-        // }
-        
-        // console.log(paladrom);
